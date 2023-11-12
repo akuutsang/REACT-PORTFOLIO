249182856a05496b7a1aa36166be6f926deeea57
akutsang <akuutsang@gmail.com>
2023-11-12 03:01:37 +0100

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center ">
-        POPUP
+        testing
       </div>
     </section>
   );
