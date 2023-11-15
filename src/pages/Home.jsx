45e593895ae1529d6aacc01fabdc8198e90892b1
akutsang <akuutsang@gmail.com>
2023-11-15 23:44:49 +0100

    add sky animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
             intensity={1.5}
           />
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island
             position={islandPosition}
             scale={islandScale}
