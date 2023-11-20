90fb0bdaad5ee56f560a7d69f00f6db3851de60a
akutsang <akuutsang@gmail.com>
2023-11-21 00:37:44 +0100

    add animations to avatar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,20 @@
 import { Canvas } from "react-three-fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Avatar } from "./Avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Happy", "Pacing", "Pointing"],
+    },
+  });
   return (
     <>
       <OrbitControls />
       <group position-y={-1}>
-        <Avatar scale={[1.2, 1.2, 1.2]} />
+        <Avatar scale={[1.2, 1.2, 1.2]} animation={animation} />
       </group>
 
       <ambientLight intensity={1} />
