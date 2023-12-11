b4a36d1c762275fbc8157466801c3c8c64c8533f
akutsang <akuutsang@gmail.com>
2023-12-11 14:08:17 +0100

    add boxGeometry

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, Sky } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 
@@ -12,11 +12,20 @@ export const Experience = () => {
   return (
     <>
       <OrbitControls />
+      <Sky />
+      <Environment preset="sunset" />
       <group position-y={-1}>
         <Avatar scale={[1.2, 1.2, 1.2]} animation={animation} />
       </group>
-
       <ambientLight intensity={1} />
+      <mesh scale={[0.8, 0.5, 0.8]} position-y={-0.65}>
+        <boxGeometry />
+        <meshStandardMaterial color="red" />
+      </mesh>
+      <mesh scale={5} rotation-x={-Math.PI * 0.2}>
+        <planeGeometry />
+        <meshStandardMaterial color="white" />
+      </mesh>
     </>
   );
 };
